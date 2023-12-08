b723c1d93333f7039e2199e48a2eca258b0f1550
TEN ALEXANDER <sasha1keshten@gmail.com>
2023-12-08 18:19:52 +0900

    added  function   goal

--- insurance/studyapp/urls.py
+++ insurance/studyapp/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
-from .views import hello ,    about
+from .views import hello ,    about , goal
 
 urlpatterns = [
     
     path('', hello, name='hello'),
-    path('about/' , about ,  name='about')
+    path('about/' , about ,  name='about'),
+    path('goal/' ,   goal  ,  name   =  'goal')
 ]
+
 
 
 
--- insurance/studyapp/views.py
+++ insurance/studyapp/views.py
@@ -9,3 +9,8 @@ def hello(request):
 
 def  about(request):
     return HttpResponse('this  is the   small  text   about  the  study  app   it   serves   to  assist  in  learning   languages ')
+
+
+
+def  goal(request):
+    return   HttpResponse('our   goal  is to build   system  that   helps you  to  learn  efficiently  in  pleasurable way ')
