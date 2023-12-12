260311c4d965aa33c16297c3f47ee25a8b20c595
TEN ALEXANDER <sasha1keshten@gmail.com>
2023-12-12 22:05:03 +0900

    created  the  model  of  student

--- insurance/studyapp/models.py
+++ insurance/studyapp/models.py
@@ -1,3 +1,11 @@
+# yourappname/models.py
+
 from django.db import models
 
-# Create your models here.
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    grade = models.CharField(max_length=2)
+
+    def __str__(self):
+        return self.name
